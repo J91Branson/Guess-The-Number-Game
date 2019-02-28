7af7c871c7ae78805da6ec30802b3825514be1a2
J91Branson <jbcod4@gmail.com>
2019-02-28 03:19:51 -0600

    20%

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+var numberToGuess = Math.floor(Math.random()*120) + 19;
+var wins = 0
+var losses = 0
+var numberToGuessText = document.getElementById("number-to-guess-text")
+var winsText = document.getElementById("wins-text")
+var lossesText = document.getElementById("losses-text")
+// Number To Guess gets shown from this:
+numberToGuessText.textContent= "Number To Guess: " + numberToGuess;
+
