0fec9aa0b1351831c6f29e87a7ad72142ed7fc5d
J91Branson <jbcod4@gmail.com>
2019-02-28 18:58:49 -0600

    50%

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,34 @@
 var numberToGuess = Math.floor(Math.random()*120) + 19;
 var wins = 0
 var losses = 0
-var numberToGuessText = document.getElementById("number-to-guess-text")
-var winsText = document.getElementById("wins-text")
-var lossesText = document.getElementById("losses-text")
+var numberToGuessText = $("#number-to-guess-text")
+var winsText = $("#wins-text")
+var lossesText = $("#losses-text")
+var buttonValue1 = $("#button1")
+var buttonValue2 = $("#button2")
+var buttonValue3 = $("#button3")
+var buttonValue4 = $("#button4")
+reset();
 // Number To Guess gets shown from this:
-numberToGuessText.textContent= "Number To Guess: " + numberToGuess;
+numberToGuessText.text("Number To Guess: " + numberToGuess);
 // Wins:
-winsText.textContent= "Wins: " + wins;
+winsText.text("Wins: " + wins);
 // Losses:
-lossesText.textContent= "Losses: " + losses;
+lossesText.text("Losses: " + losses);
+// Random number assigned to buttons each time:
+function reset(){
+    numberToGuess = Math.floor(Math.random()*120) + 19;
+    numberToGuessText.text("Number To Guess: " + numberToGuess);
+    buttonNumber1 = Math.floor(Math.random()*12) + 1;
+    buttonNumber2 = Math.floor(Math.random()*12) + 1;
+    buttonNumber3 = Math.floor(Math.random()*12) + 1;
+    buttonNumber4 = Math.floor(Math.random()*12) + 1;
+    buttonValue1.attr ("data-buttonPoints", buttonNumber1);
+    buttonValue2.attr ("data-buttonPoints", buttonNumber2);
+    buttonValue3.attr ("data-buttonPoints", buttonNumber3);
+    buttonValue4.attr ("data-buttonPoints", buttonNumber4);
+}
+
+
 
 
